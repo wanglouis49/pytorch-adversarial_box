5821e1950caaa8e40130a123b25bc07477e54a9a
Luyu Wang <wanglouis49@gmail.com>
2018-01-15 16:34:13 -0500

    adv train

---
+++ mnist_adv_train.py
@@ -0,0 +1,87 @@
+"""
+Adversarially train LeNet-5
+"""
+
+import sys
+
+import torch
+import torch.nn as nn
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+from adversarialbox.attacks import FGSMAttack, LinfPGDAttack
+from adversarialbox.train import adv_train, FGSM_train_rnd
+from adversarialbox.utils import to_var, pred_batch, test
+
+from models import LeNet5
+
+
+# Hyper-parameters
+param = {
+    'batch_size': 128,
+    'test_batch_size': 100,
+    'num_epochs': 15,
+    'delay': 10,
+    'learning_rate': 1e-3,
+}
+
+
+# Data loaders
+train_dataset = datasets.MNIST(root='../data/',train=True, download=True, 
+    transform=transforms.ToTensor())
+loader_train = torch.utils.data.DataLoader(train_dataset, 
+    batch_size=param['batch_size'], shuffle=True)
+
+test_dataset = datasets.MNIST(root='../data/', train=False, download=True, 
+    transform=transforms.ToTensor())
+loader_test = torch.utils.data.DataLoader(test_dataset, 
+    batch_size=param['test_batch_size'], shuffle=True)
+
+
+# Setup the model
+net = LeNet5()
+
+if torch.cuda.is_available():
+    print('CUDA ensabled.')
+    net.cuda()
+net.train()
+
+# Adversarial training setup
+adversary = FGSMAttack(epsilon=0.3)
+#adversary = LinfPGDAttack()
+
+# Train the model
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.RMSprop(net.parameters(), lr=param['learning_rate'])
+
+for epoch in range(param['num_epochs']):
+
+    print('Starting epoch %d / %d' % (epoch + 1, param['num_epochs']))
+
+    for t, (x, y) in enumerate(loader_train):
+
+        x_var, y_var = to_var(x), to_var(y.long())
+        loss = criterion(net(x_var), y_var)
+
+        # adversarial training
+        if epoch+1 > param['delay']:
+            # use predicted label to prevent label leaking
+            y_pred = pred_batch(x, net)
+            x_adv = adv_train(x, y_pred, net, criterion, adversary)
+            x_adv_var = to_var(x_adv)
+            loss_adv = criterion(net(x_adv_var), y_var)
+            loss = (loss + loss_adv) / 2
+
+        if (t + 1) % 100 == 0:
+            print('t = %d, loss = %.8f' % (t + 1, loss.data[0]))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+test(net, loader_test)
+
+torch.save(net.state_dict(), 'adv_trained_lenet5.pkl')
---
+++ models.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+
+class LeNet5(nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1, stride=1)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.maxpool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=1)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.maxpool2 = nn.MaxPool2d(2)
+        self.linear1 = nn.Linear(7*7*64, 200)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.linear2 = nn.Lienar(200, 10)
+
+    def forward(self, x):
+        out = self.maxpool1(self.relu1(self.conv1(x)))
+        out = self.maxpool2(self.relu2(self.conv2(out)))
+        out = out.view(out.size(0), -1)
+        out = self.relu3(self.linear1(out))
+        out = self.linear2(out)
+        return out
+
+
+class SubstituteModel(nn.Module):
+
+    def __init__(self):
+        super(SubstituteModel, self).__init__()
+        self.linear1 = nn.Linear(28*28, 200)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.linear2 = nn.Linear(200, 200)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.linear3 = nn.Linear(200, 10)
+
+    def forward(self, x):
+        out = x.view(x.size(0), -1)
+        out = self.relu1(self.linear1(out))
+        out = self.relu2(self.linear2(out))
+        out = self.linear3(out)
+        return out
